50bbde2bfabfe88e59ed28ae2c43c21e5acb6d3f
SalvatoreC <caccamosalvatore@virgilio.it>
2015-06-07 22:00:45 +0200

    Create plot2.R

---
+++ plot2.R
@@ -0,0 +1,19 @@
+# the local file is read
+ocalFile <- "./household_power_consumption.txt"
+fullData <- read.table(localFile, sep = ";", na.strings = "?", header = TRUE)
+
+# data of interest are selected
+myData <- fullData[fullData$Date == "1/2/2007" | fullData$Date == "2/2/2007",]
+
+# plot2 is produced on the screen device
+tmp <- paste(myData$Date, myData$Time)
+asseX <- strptime(tmp, format = "%d/%m/%Y %H:%M:%S", tz = " ")
+
+asseY <- as.numeric(as.character(myData$Global_active_power))
+
+plot(asseX, asseY, type = "l", xlab = "datetime", ylab = "Global Active Power (kilowatts)")
+
+# plot2 is produced on the png device
+png(file = "plot2.png")
+plot(asseX, asseY, type = "l", xlab = "datetime", ylab = "Global Active Power (kilowatts)")
+dev.off()
